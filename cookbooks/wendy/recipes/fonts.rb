21faed586878cad06167ef2b711c581877dbb685
Gene Wood <gene_wood@cementhorizon.com>
2018-04-30 14:26:58 -0700

    Add fonts

--- cookbooks/wendy/recipes/fonts.rb
+++ cookbooks/wendy/recipes/fonts.rb
@@ -17,11 +17,29 @@ end
 font_package_filename = "#{Chef::Config['file_cache_path']}/source-code-pro.zip"
 remote_file font_package_filename do
   source 'https://github.com/adobe-fonts/source-code-pro/archive/2.010R-ro/1.030R-it.zip'
-  checksum 'a5c011502ad3bc628bb4fc3646cd387482819bd2a7182162cf15095fdce3d18c'
+  checksum '3bed70c2eac576d85673204919ae4b16fadf4a70c65f80360e0dc0eb820ff4a4'
   notifies :run, 'execute[load fonts]', :immediately
 end
 
 execute "load fonts" do
-  command "unzip #{font_package_filename} source-code-pro*/OTF/*.otf -d #{base_homedir}/.fonts && fc-cache --force --verbose"
+  command "unzip #{font_package_filename} source-code-pro*/OTF/*.otf -d #{base_homedir}/.fonts"
+  notifies :run, 'execute[update font cache]', :delayed
+  action :nothing
+end
+
+for font in [
+    'Friz Quadrata Bold BT.ttf',
+    'OpenSans-Light.ttf',
+    'Zapf Calligraphic 801 BT.ttf'] do
+  cookbook_file "#{base_homedir}/.fonts/#{font}" do
+    source "fonts/#{font}"
+    owner node['base_user']['username']
+    group node['base_user']['username']
+    notifies :run, 'execute[update font cache]', :delayed
+  end
+end
+
+execute "update font cache" do
+  command "fc-cache --force --verbose"
   action :nothing
 end
