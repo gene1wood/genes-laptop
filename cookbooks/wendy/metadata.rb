743dac10a0b66e573597860f97af1c4ba09f35af
Gene Wood <gene_wood@cementhorizon.com>
2018-06-18 15:18:09 -0700

    Use newer git
    Use git from ppa
    Enable gpg signing
    Configure git to use gpg2
    Install newer Smartgit
    Install git-crypt

--- cookbooks/wendy/metadata.rb
+++ cookbooks/wendy/metadata.rb
@@ -6,10 +6,11 @@ description      'Installs/Configures wendy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends          'git', '~> 4.3'
+#depends          'git', '~> 4.3'
+depends          'git', '~> 6.0.0'
 depends          'apt'
 depends          'firewall'
-depends          'desktop'
+#depends          'desktop'
 depends          'apache2'
 depends          'poise-python', '~> 1.6.0'
 
--- cookbooks/wendy/recipes/git.rb
+++ cookbooks/wendy/recipes/git.rb
@@ -9,7 +9,20 @@
 
 base_homedir = node['etc']['passwd'][node['base_user']['username']]['dir']
 
-include_recipe 'git'
+# http://askubuntu.com/a/571633/14601
+apt_repository 'git-core-ppa' do
+  uri 'ppa:git-core/ppa'
+  distribution node['lsb']['codename']
+end
+
+# package 'git'
+git_client 'default' do
+  package_action :upgrade
+  # Ubuntu git package versions for some reason begin with "1:"
+  # This installs the newest git 2 version that begins with "1:2"
+  # https://github.com/chef/chef/issues/5578
+  package_version "1:2\*"
+end
 
 template "#{base_homedir}/.gitconfig" do
   source "gitconfig.erb"
@@ -19,7 +32,8 @@ template "#{base_homedir}/.gitconfig" do
   variables({
       :fullname => node['base_user']['fullname'],
       :email => node['base_user']['email'],
-      :username => node['base_user']['username']
+      :username => node['base_user']['username'],
+      :signingkey => node['gpg']['default_key']
   })
 end
 
@@ -31,8 +45,8 @@ cookbook_file "#{base_homedir}/.gitignore_global" do
 end
 
 remote_dpkg 'smartgit' do
-  source "http://www.syntevo.com/downloads/smartgit/smartgit-7_1_3.deb"
-  checksum "3c8442438131537f0c2e8a6656ea0827140238bbc2a3b04ff45e0f68afef3e21"
+  source "http://www.syntevo.com/static/smart/download/smartgit/smartgit-17_1_0.deb"
+  checksum "c3c1ee59252663d66f4c6b8fdc2da161d827c3da9245bbe30dbdf856beb7a8d0"
 end
 
 
@@ -42,15 +56,26 @@ directory "#{base_homedir}/.smartgit" do
   mode "0775"
 end
 
-directory "#{base_homedir}/.smartgit/6.5" do
+directory "#{base_homedir}/.smartgit/17" do
   owner node['base_user']['username']
   group node['base_user']['username']
   mode "0775"
 end
 
-file "#{base_homedir}/.smartgit/6.5/license" do
+file "#{base_homedir}/.smartgit/17/license" do
   content node['smartgit']['license']
   owner node['base_user']['username']
   group node['base_user']['username']
   mode "0640"
+end
+
+gitcrypt_filename = 'git-crypt_0.5.0-1_amd64.deb'
+gitcrypt_version = '0.5.0-1'
+cookbook_file "#{Chef::Config['file_cache_path']}/#{gitcrypt_filename}" do
+  source gitcrypt_filename
+end
+
+dpkg_package 'git-crypt' do
+  source "#{Chef::Config['file_cache_path']}/#{gitcrypt_filename}"
+  version gitcrypt_version
 end
