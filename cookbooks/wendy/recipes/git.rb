555ca4e87c461c66ba46a70bfddfecf263fe0b33
Gene Wood <gene_wood@cementhorizon.com>
2019-01-19 13:51:30 -0800

    Update git for 18.04

--- cookbooks/wendy/recipes/git.rb
+++ cookbooks/wendy/recipes/git.rb
@@ -9,23 +9,27 @@
 
 base_homedir = node['etc']['passwd'][node['base_user']['username']]['dir']
 
-# http://askubuntu.com/a/571633/14601
-apt_repository 'git-core-ppa' do
-  uri 'ppa:git-core/ppa'
-  distribution node['lsb']['codename']
-end
+if node["platform_version"] == "16.04"
+  # http://askubuntu.com/a/571633/14601
+  apt_repository 'git-core-ppa' do
+    uri 'ppa:git-core/ppa'
+    distribution node['lsb']['codename']
+  end
 
-# package 'git'
-git_client 'default' do
-  package_action :upgrade
-  # Ubuntu git package versions for some reason begin with "1:"
-  # This installs the newest git 2 version that begins with "1:2"
-  # https://github.com/chef/chef/issues/5578
-  package_version "1:2\*"
+  git_client 'default' do
+    package_action :upgrade
+    # Ubuntu git package versions for some reason begin with "1:"
+    # This installs the newest git 2 version that begins with "1:2"
+    # https://github.com/chef/chef/issues/5578
+    package_version "1:2\*"
+  end
+end
+if node["platform_version"] == "18.04"
+  package 'git'
 end
 
 template "#{base_homedir}/.gitconfig" do
-  source "gitconfig.erb"
+  source "homedir/.gitconfig.erb"
   owner node['base_user']['username']
   group node['base_user']['username']
   mode "0664"
@@ -33,22 +37,22 @@ template "#{base_homedir}/.gitconfig" do
       :fullname => node['base_user']['fullname'],
       :email => node['base_user']['email'],
       :username => node['base_user']['username'],
-      :signingkey => node['gpg']['default_key']
+      :signingkey => node['gpg']['default_key'],
+      :gpg_binary_name => node["platform_version"] == "16.04" ? 'gpg2' : 'gpg'
   })
 end
 
 cookbook_file "#{base_homedir}/.gitignore_global" do
-  source "gitignore_global"
+  source "homedir/.gitignore_global"
   owner node['base_user']['username']
   group node['base_user']['username']
   mode "0644"
 end
 
 remote_dpkg 'smartgit' do
-  source "http://www.syntevo.com/static/smart/download/smartgit/smartgit-17_1_0.deb"
-  checksum "c3c1ee59252663d66f4c6b8fdc2da161d827c3da9245bbe30dbdf856beb7a8d0"
+  source "https://www.syntevo.com/downloads/smartgit/archive/smartgit-17_1_6.deb"
+  checksum "8982cd19165794653528012b0c1e9ee8de203585cd09bde6ed55035e88a63688"
 end
-
 
 directory "#{base_homedir}/.smartgit" do
   owner node['base_user']['username']
@@ -78,4 +82,21 @@ end
 dpkg_package 'git-crypt' do
   source "#{Chef::Config['file_cache_path']}/#{gitcrypt_filename}"
   version gitcrypt_version
+  not_if "dpkg -s #{@name}"
 end
+
+require "yaml"
+
+directory "#{base_homedir}/.github" do
+  owner node['base_user']['username']
+  group node['base_user']['username']
+end
+
+file "#{base_homedir}/.github/fork_github_repo.yaml" do
+  content YAML::dump(YAML::dump(node[:github]['fork-github-repo'].to_hash).gsub('!map:Mash',''))
+  owner node['base_user']['username']
+  group node['base_user']['username']
+  mode "0600"
+end
+
+# MANUAL : Set SmartGit to "Sign All Commits"
