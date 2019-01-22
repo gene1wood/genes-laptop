3f86b0359fa3627da8fd429da24f64d305d8369e
Gene Wood <gene_wood@cementhorizon.com>
2019-01-22 08:43:15 -0800

    Add support for classic in snap custom resource

--- cookbooks/wendy/resources/snap.rb
+++ cookbooks/wendy/resources/snap.rb
@@ -1,18 +1,19 @@
 resource_name :snap
 
 property :package_name, String, required: true, name_property: true
+property :classic, [true, false], default: false
 
 default_action :install
 
 action :upgrade do
-  execute "snap install #{new_resource.package_name}" do
+  execute "snap install #{new_resource.package_name} #{new_resource.classic ? '--classic' : ''}" do
     not_if "snap list #{new_resource.package_name}"
   end
   execute "snap refresh #{new_resource.package_name}"
 end
 
 action :install do
-  execute "snap install #{new_resource.package_name}" do
+  execute "snap install #{new_resource.package_name} #{new_resource.classic ? '--classic' : ''}" do
     not_if "snap list #{new_resource.package_name}"
   end
 end
